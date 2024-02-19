c53a011bbb35883cbd41c01ec1e19f5eee712b6e
Joanne <115743659+joannefan@users.noreply.github.com>
2024-02-19 14:21:45 -0500

    add dish details, notify button without api

--- frontend/app/dish.js
+++ frontend/app/dish.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import axios from "axios";
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 
 const NutritionTable = ({ nutritionInfo }) => {
   return (
@@ -18,22 +19,118 @@ const NutritionTable = ({ nutritionInfo }) => {
   );
 };
 
+const IngredientList = ({ ingredients, allergens }) => {
+  propertyEmoji = {
+    "coconut": "🥥", 
+    "egg": "🥚", 
+    "fish": "🐟",
+    "gluten": "🌾",
+    "halal": "?",
+    "milk": "🥛",
+    "peanut": "🥜",
+    "sesame": "",
+    "shellfish": "🍤",
+    "soy": "🫛",
+    "tree-nuts": "🌰",
+    "vegan": "",
+    "vegetarian": ""
+  }
+
+  return (
+    <View style={styles.paragraph}>
+      <Text style={styles.subtitle}>Ingredients: </Text>
+      <Text>{ingredients}</Text>
+      <View style={styles.allergensContainer}>
+        {allergens.map((entry, index) => (
+          <View key={index} style={styles.allergenBox}>
+            <Text style={styles.allergenText}>{propertyEmoji[entry]} {entry}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  )
+}
+
+const NotifyBtn = ({ dishHash }) => {
+  const [buttonText, setButtonText] = useState('Notify Me!');
+  const [backgroundColor, setBackgroundColor] = useState('red');
+
+  const handlePress = () => {
+    setTimeout(() => {
+      console.log("dish hash: " + dishHash);
+      const response = { status: 200 }; // replace with actual API call
+
+      if (response.status === 200) {
+        setButtonText('Subscribed!');
+        setBackgroundColor('green');
+      }
+    }, 1000); // simulating API call delay
+  };
+
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <View style={[styles.button, { backgroundColor }]}>
+        <Text style={styles.buttonText}>{buttonText}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
 const DishScreen = ({ route }) => {
   const { menuItem } = route.params; 
 
   return (
-    <View>
-      <Text>Nutrition Profile of {menuItem.name}</Text>
-      <NutritionTable nutritionInfo={menuItem.nutrition_information}></NutritionTable>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>{menuItem.name}</Text>
+      <IngredientList ingredients={menuItem.ingredients} allergens={menuItem.food_properties}/>
+      <NutritionTable nutritionInfo={menuItem.nutrition_information}/>
+      <NotifyBtn dishHash={menuItem.hash}/>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    fontStyle: 'italic',
+    marginBottom: 4,
+  },
+  paragraph: {
+    paddingBottom: 10,
+  },
+  allergensContainer: {
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+  allergenBox: {
+    width: 80,
+    height: 40,
+    backgroundColor: '#517C50',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+  allergenText: {
+    color: "white"
+  },
   table: {
     borderWidth: 1,
-    borderColor: '#000000',
+    borderColor: '#87725A',
     margin: 10,
+    backgroundColor: "#F3DACE"
   },
   tableRow: {
     flexDirection: 'row',
@@ -41,8 +138,8 @@ const styles = StyleSheet.create({
   cell: {
     flex: 1,
     padding: 6,
-    borderWidth: 1,
-    borderColor: '#000000',
+    borderWidth: 0.5,
+    borderColor: '#87725A',
   },
   header: {
     fontWeight: 'bold',
